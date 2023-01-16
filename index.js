1f897c4a275f515fd1c82eb7a9d6aa05aab7980d
Felix-Josephraj <felixanderson500@gmail.com>
2023-01-16 23:48:24 +0530

    bookmyshow added

--- index.js
+++ index.js
@@ -43,7 +43,31 @@ app.get('/api', async (req, res) => {
     let browser = await puppeteer.launch(options)
 
     let page = await browser.newPage()
-    await page.goto('https://www.google.com')
+    await page.goto(
+      // 'https://www.flipkart.com/'
+      'https://in.bookmyshow.com/trichy/cinemas/ramba-ac-dolby-atmos-trichy/RAMB/20230119'
+      // 'https://in.bookmyshow.com/trichy/cinemas/la-cinema-maris-complex-rgb-laser-trichy/LATG/20230117'
+    )
+
+    /* Run javascript inside the page */
+    const data = await page.evaluate(() => {
+      const list = []
+      const items = document.querySelector('title').innerHTML
+
+      // for (const item of items) {
+      //   list.push({
+      //     company: item.querySelector('.company h3').innerHTML,
+      //     position: item.querySelector('.company h2').innerHTML,
+      //     link: 'https://remoteok.io' + item.getAttribute('data-href'),
+      //   })
+      // }
+
+      return items
+
+      // return list
+    })
+    await browser.close()
+    res.send(data)
     // res.send(await page.title());
 
     // updating the database
@@ -53,24 +77,24 @@ app.get('/api', async (req, res) => {
     // }
 
     let count = 0
-    var intervalId = setInterval(function () {
-      // call your function here
-      count += 1
-      const eatBurger = new Item({
-        name: 'Eat burger' + count,
-      })
+    // var intervalId = setInterval(function () {
+    //   // call your function here
+    //   count += 1
+    //   const eatBurger = new Item({
+    //     name: 'Eat burger' + count,
+    //   })
 
-      Item.insertMany([eatBurger], function (err) {
-        if (err) {
-          // console.log(err)
-        } else {
-          // console.log('Successfully inserted')
-        }
-      })
-      if (count >= 5) {
-        clearInterval(intervalId)
-      }
-    }, 30000)
+    //   Item.insertMany([eatBurger], function (err) {
+    //     if (err) {
+    //       // console.log(err)
+    //     } else {
+    //       // console.log('Successfully inserted')
+    //     }
+    //   })
+    //   if (count >= 5) {
+    //     clearInterval(intervalId)
+    //   }
+    // }, 30000)
 
     // function task(i) {
     //   const eatBurger = new Item({
@@ -91,7 +115,6 @@ app.get('/api', async (req, res) => {
     console.error(err)
     return null
   }
-  res.send('gaas')
 })
 
 app.listen(process.env.PORT || 3000, () => {
