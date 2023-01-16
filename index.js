4cefb23c6efdc48af6efaef5c3b0ca706787ef79
Felix-Josephraj <felixanderson500@gmail.com>
2023-01-16 21:41:59 +0530

    Second commit

--- index.js
+++ index.js
@@ -1,42 +1,43 @@
-const app = require("express")();
+const app = require('express')()
 
-let chrome = {};
-let puppeteer;
+let chrome = {}
+let puppeteer
 
 if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-  chrome = require("chrome-aws-lambda");
-  puppeteer = require("puppeteer-core");
+  chrome = require('chrome-aws-lambda')
+  puppeteer = require('puppeteer-core')
 } else {
-  puppeteer = require("puppeteer");
+  puppeteer = require('puppeteer')
 }
 
-app.get("/api", async (req, res) => {
-  let options = {};
+app.get('/api', async (req, res) => {
+  let options = {}
 
   if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
     options = {
-      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+      args: [...chrome.args, '--hide-scrollbars', '--disable-web-security'],
       defaultViewport: chrome.defaultViewport,
       executablePath: await chrome.executablePath,
       headless: true,
       ignoreHTTPSErrors: true,
-    };
+    }
   }
 
   try {
-    let browser = await puppeteer.launch(options);
+    let browser = await puppeteer.launch(options)
 
-    let page = await browser.newPage();
-    await page.goto("https://www.google.com");
-    res.send(await page.title());
+    let page = await browser.newPage()
+    await page.goto('https://www.google.com')
+    // res.send(await page.title());
+    res.send('gaas')
   } catch (err) {
-    console.error(err);
-    return null;
+    console.error(err)
+    return null
   }
-});
+})
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log("Server started");
-});
+  console.log('Server started')
+})
 
-module.exports = app;
+module.exports = app
